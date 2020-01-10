17a2ab8e16ade3b0404e55ab1baad76168653be9
Peter Mazuryk <petermazuryk1984@gmail.com>
2020-01-11 00:56:20 +0200

    Updates

---
+++ precache-manifest.3abb27471374dbebd3b6ec5b672c234b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d27fb2ef08c57f2b88ff763aa3ade3e",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "ff5752bb1113d6def3a2",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.dc3aa949.chunk.css"
+  },
+  {
+    "revision": "2b59e4674c335c5629b5",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6e70b7da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6e70b7da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff5752bb1113d6def3a2",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.e15cb33a.chunk.js"
+  },
+  {
+    "revision": "13285e73bee095c5d415",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.22254703.js"
+  }
+]);
